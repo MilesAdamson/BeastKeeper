99634aa8478d523dec4cf8027f19714a2bc803d3
Miles Adamson <milesadamson@hotmail.com>
2019-02-08 22:48:48 -0700

    Added a menu resource file for ProfileActivity, and the onCreateOptionsMenu and onOptionsItemSelected Override methods. Added camera button to ActionBar.

--- app/src/main/java/com/adamson/miles/beastkeeper/ProfileActivity.java
+++ app/src/main/java/com/adamson/miles/beastkeeper/ProfileActivity.java
@@ -1,7 +1,11 @@
 package com.adamson.miles.beastkeeper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,4 +14,19 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
